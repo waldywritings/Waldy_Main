4458a486801f57a1ecfae3e922f383a783963a0a
bittukittu123 <131584591+bittukittu123@users.noreply.github.com>
2023-06-13 06:19:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,9 +30,9 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://aegonjohnsnow2:Manideep2@cluster0.pplqzdv.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://daaaavudaaaa_info1:daaaavudaaaa_info1@cluster0.ookasqc.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "manipspkfilesbot")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_filess')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-10019277289837'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'ph_files')
 
