52bd9060766d09895703ec52ff711ae43937987f
bittukittu123 <131584591+bittukittu123@users.noreply.github.com>
2023-06-13 06:23:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL','-1001283160523')
+auth_channel = environ.get('AUTH_CHANNEL','-1001583857001')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
 
