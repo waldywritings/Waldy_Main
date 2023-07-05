035d89fc3899353efa487cc403e5001c0861f95a
WaldyNishi2 <138442977+WaldyNishi2@users.noreply.github.com>
2023-07-05 08:14:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -84,7 +84,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}]-💠-{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"[{get_size(file.file_size)}]-🔥✨-{file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
